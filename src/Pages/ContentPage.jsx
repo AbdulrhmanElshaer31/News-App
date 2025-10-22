db4e047f37048165e233620a4756673fc51f7f46
bido <bidoraafat@gmail.com>
2025-10-22 19:08:02 +0300

    content page edit

--- src/Pages/ContentPage.jsx
+++ src/Pages/ContentPage.jsx
@@ -1,14 +1,20 @@
 import Card from "../components/Card";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+
 export default function ContentPage() {
   return (
-    <div className="relative min-h-screen bg-gray-200">
-      <Header />
-    <div className=" h-[2px]  bg-gray-500"></div>
-      <div className="flex pt-20 px-10 gap-10">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex">
+        {/* Sidebar */}
         <Sidebar />
-        <Card />
+
+        {/* Main content */}
+        <div className="bg-[#ECEFF4]/70 backdrop-blur-xl rounded-2xl shadow-lg">
+          <Header />
+          <div className="h-[2px] bg-gray-500 my-2"></div>
+          <Card />
+        </div>
       </div>
     </div>
   );
