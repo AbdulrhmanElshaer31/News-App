4c04b7561df14aaf7e4799aa89b1e03a0d2b3f74
AbdulrhmanElshaer31 <abdelrhmanelshaer31@gmail.com>
2025-10-30 19:36:35 +0300

    (Everything-API Added!)

--- src/api/newsApi.js
+++ src/api/newsApi.js
@@ -1,15 +1,44 @@
-import axios from "axios";
+class NewsApi {
+  #base_url;
+  #api_key;
 
-const getBreakingNews = async (query) => {
-  try {
-    const res = await axios.get(
-      `https://newsapi.org/v2/everything?q=${query.toLowerCase()}&apiKey=0370bfd5e5b4406c917242d5f09cb200`
-    );
-    return res.data;
-  } catch (error) {
-    console.error("Error fetching news:", error);
-    return null;
+  constructor(base_url, api_key) {
+    this.#base_url = base_url;
+    this.#api_key = api_key;
   }
-};
 
-export default getBreakingNews;
+  async getEverything(search) {
+    if (!search) {
+      console.warn("Missing search parameter!");
+      return [];
+    }
+
+    try {
+      const url = new URL(this.#base_url);
+      url.searchParams.set("q", search);
+      url.searchParams.set("apiKey", this.#api_key);
+
+      const request = await fetch(url);
+
+      if (!request.ok) {
+        console.error(`HTTP Request Failed! Status: ${request.status}`);
+        return [];
+      }
+
+      const response = await request.json();
+      console.log("Data Loaded Correctly", response);
+      return response;
+
+    } catch (err) {
+      console.error("Failed To Fetch API:", err.message);
+      return [];
+    }
+  }
+}
+
+
+const news = new NewsApi("https://newsapi.org/v2/everything","944cbb0de3bd43a289e583e42cfeeb34");
+
+
+//How To Use My Friend 3abedo
+news.getEverything("tesla")
